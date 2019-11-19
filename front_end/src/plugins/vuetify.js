b1da53cd1750d1be2834167218d3f2783b03bece
gmhdbjd <gmhdbjd@gmail.com>
2019-11-18 21:51:11 -0500

    change theme

--- front_end/src/plugins/vuetify.js
+++ front_end/src/plugins/vuetify.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.teal.base,
+        secondary: colors.blue.base,
+        accent: colors.indigo.base
+      },
+      dark: {
+        primary: colors.teal.base,
+        secondary: colors.blue.base,
+        accent: colors.indigo.base
+      }
+    }
+  }
+})
