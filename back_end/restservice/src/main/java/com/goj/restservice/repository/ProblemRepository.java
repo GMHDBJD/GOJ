d3c2d0db4f5f8fcc99c3749d0fceedb4037958b0
gmhdbjd <gmhdbjd@gmail.com>
2020-02-10 23:42:38 -0500

    change to PagingAndSortingRepository

--- back_end/restservice/src/main/java/com/goj/restservice/repository/ProblemRepository.java
+++ back_end/restservice/src/main/java/com/goj/restservice/repository/ProblemRepository.java
@@ -1,12 +1,15 @@
 package com.goj.restservice.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.goj.restservice.entity.Problem;
+import com.goj.restservice.projection.ProblemProjection;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface ProblemRepository extends CrudRepository<Problem, Integer> {
-
+public interface ProblemRepository extends PagingAndSortingRepository<Problem, Long> {
+    Page<ProblemProjection> findAllProblemProjectionBy(Pageable pageable);
 }
