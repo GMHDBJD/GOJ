9a8c7f0c1bf651446e326aa0ff0540c2f20f672c
gmhdbjd <gmhdbjd@gmail.com>
2020-02-13 03:52:13 -0500

    add config

---
+++ back_end/restservice/src/main/java/com/goj/restservice/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.goj.restservice.config;
+
+import com.goj.restservice.security.JwtConfigurer;
+import com.goj.restservice.security.JwtTokenProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable().csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                /*
+                 * .authorizeRequests().and().apply(new JwtConfigurer(jwtTokenProvider));
+                 * .antMatchers("/auth/signin").permitAll(). antMatchers(HttpMethod.GET,
+                 * "/vehicles/**").permitAll() .antMatchers(HttpMethod.DELETE,
+                 * "/vehicles/**").hasRole("ADMIN") .antMatchers(HttpMethod.GET,
+                 * "/v1/vehicles/**").permitAll() .anyRequest().authenticated() .and()
+                 */
+                .apply(new JwtConfigurer(jwtTokenProvider));
+
+    }
+}
