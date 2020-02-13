2573a4818fb1d5338deb73a66b6820f3d470bd10
gmhdbjd <gmhdbjd@gmail.com>
2020-02-13 03:56:05 -0500

    change UserRepository

--- back_end/restservice/src/main/java/com/goj/restservice/repository/UserRepository.java
+++ back_end/restservice/src/main/java/com/goj/restservice/repository/UserRepository.java
@@ -2,11 +2,17 @@ package com.goj.restservice.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 import com.goj.restservice.entity.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
