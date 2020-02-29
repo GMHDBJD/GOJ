d6cb1038b19f786e97fd40c31b8e286dc4b283d1
gmhdbjd <gmhdbjd@gmail.com>
2020-02-29 09:07:10 -0500

    update judger

--- judger/judge.py
+++ judger/judge.py
@@ -1,16 +1,8 @@
 import os
-import sys
 import time
 import subprocess
 import redis
 import json
-
-
-def file_read(filename):
-    f = open(filename, "r", encoding="utf-8")
-    d = f.read()
-    f.close()
-    return d.replace("\r", "")
 
 
 class Judge():
@@ -25,15 +17,16 @@ class Judge():
             'time': None,
             'memory': None,
             'result': None,
-            'submissionId': None
+            'submissionId': None,
+            'codeLength': None
         }
 
         self.languages = {
-            'c': {'extension': 'c', 'compile': 'cc env/code.c -O2 -fomit-frame-pointer -o env/code', 'execute': 'env/code[inputfile]'},
-            'c++': {'extension': 'cpp', 'compile': 'g++ env/code.cpp -O2 -fomit-frame-pointer -o env/code', 'execute': 'env/code[inputfile]'},
-            'java': {'extension': 'java', 'compile': 'javac -g:none -Xlint -d env env/code.java', 'execute': 'java -client -classpath env code[inputfile]'},
-            'python': {'extension': 'py', 'execute': 'python2 env/code.py[inputfile]'},
-            'python3': {'extension': 'py',  'execute': 'python3 env/code.py[inputfile]'}
+            'c': {'extension': 'c', 'compile': 'cc env/Main.c -O2 -fomit-frame-pointer -o env/Main', 'execute': 'env/Main[inputfile]'},
+            'c++': {'extension': 'cpp', 'compile': 'g++ env/Main.cpp -O2 -fomit-frame-pointer -o env/Main', 'execute': 'env/Main[inputfile]'},
+            'java': {'extension': 'java', 'compile': 'javac -g:none -Xlint -d env env/Main.java', 'execute': 'java -client -classpath env Main[inputfile]'},
+            'python': {'extension': 'py', 'execute': 'python2 env/Main.py[inputfile]'},
+            'python3': {'extension': 'py',  'execute': 'python3 env/Main.py[inputfile]'}
         }
 
     def runForever(self):
@@ -48,39 +41,38 @@ class Judge():
             self.code = submission['code']
             self.problemId = submission['problemId']
             self.language = submission['language']
-            self.codeFilename = 'code'
+            self.codeFilename = 'Main'
             self.timeLimit = submission['timeLimit']
-            self.timeLimit = 1000
             self.memoryLimit = submission['memoryLimit']
 
             self.result['submissionId'] = self.submissionId
+            self.result['codeLength'] = len(self.code)
 
             self.judge()
 
     def create(self):
         os.system('rm -r env/*')
         codeFile = open(
-            "env/code." + self.languages[self.language]["extension"], "w", encoding="utf-8")
+            "env/Main." + self.languages[self.language]["extension"], "w", encoding="utf-8")
         codeFile.write(self.code.replace("\r", ""))
         codeFile.close()
 
     def compile(self):
-        if(self.language not in ('c', 'c++', 'java')):
+        if self.language not in ('c', 'c++', 'java'):
             return
 
         compilecmd = self.languages[self.language]['compile']
-        print(compilecmd)
         os.system(compilecmd)
 
     def execute(self):
 
         if self.language == 'java':
             if not os.path.exists('env/'+self.codeFilename+'.class'):
-                self.result['result'] = 'CE'
+                self.result['result'] = 1
                 return
-        else:
+        elif self.language in ('c', 'c++'):
             if not os.path.exists('env/'+self.codeFilename):
-                self.result['result'] = 'CE'
+                self.result['result'] = 1
                 return
 
         for directory in os.listdir('iocache/'+str(self.problemId)):
@@ -93,16 +85,20 @@ class Judge():
 
             inputfile = ' <'+inputFilename+' 1 > '+resultFilename
 
-            os.system("chmod 100 .")
+            if self.language == 'java':
+                os.system("chmod 500 .")
+            else:
+                os.system("chmod 100 .")
 
-            cmd = 'ulimit -v '+str(self.memoryLimit)+'; su judge -c \"' +  self.languages[self.language]["execute"]+"; exit;\""
+            cmd = 'ulimit -v '+str(self.memoryLimit)+'; su judge -c \"' + \
+                self.languages[self.language]["execute"]+"; exit;\""
 
             cmd = cmd.replace("[inputfile]", inputfile)
 
             os.system("chmod 777 "+inputFilename)
             os.system("chmod 777 "+outputFilename)
-            os.system("chmod 777 "+resultFilename)
 
+            os.system("ls env/")
             starttime = time.time()
             proc = subprocess.Popen([cmd], shell=True, preexec_fn=os.setsid)
             try:
@@ -110,7 +106,7 @@ class Judge():
                 t = proc.returncode
             except subprocess.TimeoutExpired:
                 self.result['time'] = self.timeLimit
-                self.result['result'] = 'TLE'
+                self.result['result'] = 3
                 return
 
             endtime = time.time()
@@ -119,17 +115,17 @@ class Judge():
             os.system("pkill -u judge")
 
             if t != 0:
-                self.result['result'] = "RTE"
+                self.result['result'] = 2
                 return
 
             self.result['time'] = int(1000*(endtime - starttime))
             self.result['memory'] = self.memoryLimit
 
             if os.system('cmp ' + outputFilename+' '+resultFilename) != 0:
-                self.result['result'] = 'WA'
+                self.result['result'] = 4
                 return
 
-        self.result['result'] = 'AC'
+        self.result['result'] = 5
 
     def judge(self):
 
@@ -137,12 +133,11 @@ class Judge():
 
         self.compile()
 
-        ''' if self.language in ('java', 'python', 'python3'):
+        if self.language in ('java', 'python', 'python3'):
             if self.language in ("java"):
                 self.timeLimit *= 2
             elif self.language in ("python", "python3"):
                 self.timeLimit *= 3
-        '''
 
         self.execute()
 
